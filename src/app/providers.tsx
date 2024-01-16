7b039a3959fb80b59a20643ba5c308855d3f2283
mozza55 <mozzarella1103@gmail.com>
2024-01-16 14:39:57 +0900

    feat: react-query provider 추가

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,23 @@
-"use client";
+'use client';
 
-import React from "react";
-import { SessionProvider } from "next-auth/react";
+import React from 'react';
+import { SessionProvider } from 'next-auth/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SessionProvider>
+  );
 };
 
 export default Providers;
